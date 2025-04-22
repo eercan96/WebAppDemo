2f0766078aa8a14791b438e8e16978c4279fef9d
Emre <emreercan755@gmail.com>
2025-04-22 18:28:16 +0300

    first website page

---
+++ WebAppSample/ETicaret/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using ETicaret.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ETicaret.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Login(Login login)
+        {
+            if (login.Email == "emre.ercan@windowslive.com")
+            {
+                return RedirectToAction("Home", "Home");
+            }
+            TempData["hata"] = "Hatalı giriş";
+            return RedirectToAction("Index", "Home");
+            
+           
+        }
+
+        [HttpGet]
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+      
+    }
+}
---
+++ WebAppSample/WebApplication2/Controllers/DefaultController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            // ViewBag.metin = "asdas";
+            TempData["metin"] = "aaa";
+            return View();
+        }
+    }
+}
