6e51e936bd3cc66281b6839c0313e2022631248c
Emre <emreercan755@gmail.com>
2025-04-23 03:59:34 +0300

    Add SQL data fetching with SqlDataAdapter

--- WebAppSample/ETicaret/Controllers/HomeController.cs
+++ WebAppSample/ETicaret/Controllers/HomeController.cs
@@ -1,11 +1,14 @@
 ﻿using ETicaret.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
 using System.Diagnostics;
 
 namespace ETicaret.Controllers
 {
     public class HomeController : Controller
     {
+        SqlConnection connection=new SqlConnection("Server=EMRE\\SQLEXPRESS01; Database=EticaretDemoDb;Integrated Security=true");
 
         [HttpGet]
         public IActionResult Index()
@@ -30,7 +33,25 @@ namespace ETicaret.Controllers
         [HttpGet]
         public IActionResult Home()
         {
-            return View();
+            SqlDataAdapter adapter = new SqlDataAdapter("Select * From Urunler",connection);
+            DataTable dataTable = new DataTable();
+            dataTable.Clear();
+            adapter.Fill(dataTable);
+            List<Urunler> urunlerim=new List<Urunler>();
+            foreach (DataRow row in dataTable.Rows)
+            {
+                urunlerim.Add(new Urunler
+                {
+                    Id = Convert.ToInt32(row["Id"]),
+                    UrunAdi = row["Urunadi"].ToString(),
+                    Resim = row["Resim"].ToString(),
+                    BirimFiyati = Convert.ToDecimal(row["BirimFiyati"])
+
+
+                });
+
+            }
+            return View(urunlerim);
         }
 
       
---
+++ WebAppSample/ETicaret/Models/Urunler.cs
@@ -0,0 +1,10 @@
+﻿namespace ETicaret.Models
+{
+    public class Urunler
+    {
+        public int Id { get; set; }
+        public string UrunAdi { get; set; }
+        public string Resim { get; set; }
+        public decimal BirimFiyati { get; set; }
+    }
+}
